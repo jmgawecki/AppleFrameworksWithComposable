6a36afec2ca1561321dd70882d5b36859d168614
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-05-20 09:32:32 +0100

    Initial Commit

---
+++ CAApp/CAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAAppApp.swift
+//  CAApp
+//
+//  Created by Jakub Gawecki on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
